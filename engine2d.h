c39b11f0a8c2412e6c187f240d378619fcca5665
Bogdan Muhanov <bogdanmuhanov95@gmail.com>
2024-10-24 18:18:46 +0300

    clear comments

--- engine2d.h
+++ engine2d.h
@@ -1,45 +1,45 @@
 #ifndef ENGINE2D_H
 #define ENGINE2D_H
 
-#include <vector>
-#include <iostream>
-#include <cmath>
 #include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <GL/glu.h>
+#include <GLFW/glfw3.h>
+#include <cmath>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-//#include <ft2build.h>
-//#include FT_FREETYPE_H
+#include <iostream>
+#include <vector>
 
-#define RED {1.0f, 0.0f, 0.0f}
-#define GREEN {0.0f, 1.0f, 0.0f}
-#define BLUE {0.0f, 0.0f, 1.0f}
-#define YELLOW {1.0f, 1.0f, 0.0f}
-#define PURPLE {0.5f, 0.0f, 0.5f}
+#define RED                                                                    \
+  { 1.0f, 0.0f, 0.0f }
+#define GREEN                                                                  \
+  { 0.0f, 1.0f, 0.0f }
+#define BLUE                                                                   \
+  { 0.0f, 0.0f, 1.0f }
+#define YELLOW                                                                 \
+  { 1.0f, 1.0f, 0.0f }
+#define PURPLE                                                                 \
+  { 0.5f, 0.0f, 0.5f }
 
 class Engine {
 private:
-    //FT_Library ft;
-    //FT_Face face;
-    GLFWwindow *window;
-    int winWidth;
-    int winHeight;
-    unsigned int shaderProgram;
+  GLFWwindow *window;
+  int winWidth;
+  int winHeight;
+  unsigned int shaderProgram;
+
 public:
-    Engine(int newWinWidth, int newWinHeight, const char* title);
+  Engine(int newWinWidth, int newWinHeight, const char *title);
+
 private:
-    //void initFreeType();
-    //FT_Face loadFont(const char* fontPath, int fontSize);
-    //void renderText(FT_Face face, const char* text, glm::vec3 posScale);
-    void initWin(const char* title);
-    void initShaders();
-    void checkStatus(unsigned int shader, unsigned int checkStatus);
-    void drawGrid(float step);
-    glm::vec2 RotVector(glm::vec2 vector, float theta);
-    void drawVector(glm::vec2 vector, glm::vec3 color);
-    void drawVector(glm::vec2 vector, glm::vec3 color, glm::vec2 origin);
-    void mainLoop();
+  void initWin(const char *title);
+  void initShaders();
+  void checkStatus(unsigned int shader, unsigned int checkStatus);
+  void drawGrid(float step);
+  glm::vec2 RotVector(glm::vec2 vector, float theta);
+  void drawVector(glm::vec2 vector, glm::vec3 color);
+  void drawVector(glm::vec2 vector, glm::vec3 color, glm::vec2 origin);
+  void mainLoop();
 };
 
 #endif
